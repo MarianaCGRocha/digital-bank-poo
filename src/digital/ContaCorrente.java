9693e944c929a820924355112d421f4f433205d3
MarianaCGRocha <marianagomesr@gmail.com>
2022-04-01 03:47:40 -0300

    Adding inheritance for classes and a IConta interface

--- src/digital/ContaCorrente.java
+++ src/digital/ContaCorrente.java
@@ -1,32 +1,5 @@
 package digital;
 
-public class ContaCorrente {
-    private int agencia;
-    private int numero;
-    private int saldo;
-
-    public void sacar() {
-
-    }
-
-    public void depositar() {
-
-    }
-
-    public void transferir() {
-
-    }
-
-    public int getAgencia() {
-        return agencia;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public int getSaldo() {
-        return saldo;
-    }
+public class ContaCorrente extends Conta {
 
 }
