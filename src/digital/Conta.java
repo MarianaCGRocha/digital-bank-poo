240834a3df06bd4cde20c637eb12be041cff67c2
MarianaCGRocha <marianagomesr@gmail.com>
2022-04-01 23:01:24 -0300

    Implementation of withdraw, transfer and deposit methods with oo

--- src/digital/Conta.java
+++ src/digital/Conta.java
@@ -2,23 +2,31 @@ package digital;
 
 public abstract class Conta implements IConta {
 
-    private int agencia;
-    private int numero;
-    private double saldo;
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+    protected static final int AGENCIA_PADRAO = 1;
+    protected static int SEQUENCIAL = 1;
+
+    public Conta() {
+        agencia = AGENCIA_PADRAO;
+        numero = SEQUENCIAL++;
+    }
 
     @Override
     public void sacar(double valor) {
-
+        saldo -= valor;
     }
 
     @Override
     public void depositar(double valor) {
-
+        saldo += valor;
     }
 
     @Override
     public void transferir(double valor, Conta contaDestino) {
-
+        this.sacar(valor);
+        contaDestino.depositar(valor);
     }
 
     public int getAgencia() {
